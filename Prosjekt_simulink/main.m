07f8ed73aa8c45aa62693c2511d21fd6c3c27d5b
Erling Hovlid <erlinhov@stud.ntnu.no>
2024-10-15 08:19:39 +0200

    Commit

--- Prosjekt_simulink/main.m
+++ Prosjekt_simulink/main.m
@@ -1,4 +1,4 @@
-time = 1200;
+time = 120;
 
 F_thruster = 111000;
 F_side_thruster = 440;
@@ -6,15 +6,20 @@ k = 0.02;
 g = 9.81;
 m = 760;
 
-dt = 4;% Distanse til thrustere
+dt = 4; % Distanse til thrustere
 ds = -1; % Distanse til sentrum
 height = 8.15;
 radius = 0.22;
-J = 1/12*m *(height^2 + 3*radius^2);
-W = 1;
-angle_ref = 0;
 
-Kp = 100;
+J = 1/12*m *(height^2 + 3*radius^2);
+
+w = -10;
+Ka = 1; % Luftmotstand
+
+angle_ref = 0;
+x_ref = 0;
+
+Kp = 0;
 Ki = 0;
 Kd = 0;
 
